d7c4b1bfacd18c7581932e8e1c4acf4369e24f8b
Armando Yanez <ayanez20@wou.edu>
2023-05-14 23:28:33 -0700

    fixed warning from RequestId being nullable

--- Project/Models/ErrorViewModel.cs
+++ Project/Models/ErrorViewModel.cs
@@ -2,7 +2,6 @@ namespace Portfolio.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
-
+    public string RequestId { get; set; }
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
