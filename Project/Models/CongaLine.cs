9ff08485e0685a9dc43a00484a1b9af61dca7ffc
Armando Yanez <ayanez20@wou.edu>
2023-05-31 19:15:41 -0700

    added CongaLine class that still needs to be tested

---
+++ Project/Models/CongaLine.cs
@@ -0,0 +1,40 @@
+namespace Portfolio.Models;
+public class CongaLine
+{
+    private List<char> Line = new List<char>();
+
+    public void Engine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Caboose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void JumpInLine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EveryOneOut()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void YourDone()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Brains()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RainbowBrains()
+    {
+        throw new NotImplementedException();
+    }
+}
