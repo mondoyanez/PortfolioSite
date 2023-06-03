61cde890e26d66179fdcd935777ce4372ffd2443
Armando Yanez <ayanez20@wou.edu>
2023-06-02 20:29:59 -0700

    Finished implementating and testing StringToListChar method in Converter class

--- Test/Converter_Tests.cs
+++ Test/Converter_Tests.cs
@@ -4,7 +4,7 @@ namespace Test;
 public class Converter_Tests
 {
     [Test]
-    public void StringToListChar_ConvertValidString_ShouldSuccessfullyReturnCorrectListOfChar()
+    public void StringToListChar_ConvertValidStringNoRepeatingCharacters_ShouldSuccessfullyReturnCorrectListOfChar()
     {
         // Arrange
         Converter converter = new();
@@ -19,6 +19,63 @@ public class Converter_Tests
             Assert.That(actual.Count, Is.EqualTo(8));
             Assert.That(actual.First(), Is.EqualTo('A'));
             Assert.That(actual.Last(), Is.EqualTo('H'));
+        });
+    }
+
+    [Test]
+    public void StringToListChar_ConvertValidStringRepeatingCharacters_ShouldSuccessfullyReturnCorrectListOfChar()
+    {
+        // Arrange
+        Converter converter = new();
+        string sampleString = "A, A, C, D, A, F, G, A";
+
+        // Act
+        List<char> actual = converter.StringToListChar(sampleString);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Count, Is.EqualTo(8));
+            Assert.That(actual.First(), Is.EqualTo('A'));
+            Assert.That(actual.Last(), Is.EqualTo('A'));
+        });
+    }
+
+    [Test]
+    public void StringToListChar_ConvertValidStringWithSameRepeatingCharacter_ShouldSuccessfullyReturnCorrectListOfChar()
+    {
+        // Arrange
+        Converter converter = new();
+        string sampleString = "A, A, A, A, A, A, A, A";
+
+        // Act
+        List<char> actual = converter.StringToListChar(sampleString);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Count, Is.EqualTo(8));
+            Assert.That(actual.First(), Is.EqualTo('A'));
+            Assert.That(actual.Last(), Is.EqualTo('A'));
+        });
+    }
+
+    [Test]
+    public void StringToListChar_ConvertValidStringWithSingleCharacter_ShouldSuccessfullyReturnCorrectListOfChar()
+    {
+        // Arrange
+        Converter converter = new();
+        string sampleString = "A";
+
+        // Act
+        List<char> actual = converter.StringToListChar(sampleString);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Count, Is.EqualTo(1));
+            Assert.That(actual.First(), Is.EqualTo('A'));
+            Assert.That(actual.Last(), Is.EqualTo('A'));
         });
     }
 }
