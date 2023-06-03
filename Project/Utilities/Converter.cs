5e32827ea66b3665518b7bc543d0545ebbc1997f
Armando Yanez <ayanez20@wou.edu>
2023-06-02 19:58:33 -0700

    Beginning implementation of StringToListChar method in Converter class using pre-existing code from CongaLine controller using TDD to help debug current bug in code

---
+++ Project/Utilities/Converter.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Utilities;
+public class Converter
+{
+    public List<char> StringToListChar(string str)
+    {
+        // return str.Split(',').ToList().SelectMany(s => s).ToList();
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Test/Converter_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Portfolio.Utilities;
+
+namespace Test;
+public class Converter_Tests
+{
+    [Test]
+    public void StringToListChar_ConvertValidString_ShouldSuccessfullyReturnCorrectListOfChar()
+    {
+        // Arrange
+        Converter converter = new();
+        string sampleString = "A, B, C, D, E, F, G, H";
+
+        // Act
+        List<char> actual = converter.StringToListChar(sampleString);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Count, Is.EqualTo(8));
+            Assert.That(actual.First(), Is.EqualTo('A'));
+            Assert.That(actual.Last(), Is.EqualTo('H'));
+        });
+    }
+}
