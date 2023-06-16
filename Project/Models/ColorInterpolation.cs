de256094acb45c827938ad7bcc8b9c96b5c41b50
Armando Yanez <ayanez20@wou.edu>
2023-06-15 22:13:56 -0700

    Implementated and tested ColorToHSV method from Color class

--- Project/Models/ColorInterpolation.cs
+++ Project/Models/ColorInterpolation.cs
@@ -6,6 +6,7 @@ public class ColorInterpolation: Color
 {
     // From Greg's answer: https://stackoverflow.com/questions/359612/how-to-change-rgb-color-to-hsv/1626175
     // And Wikipedia: https://en.wikipedia.org/wiki/HSL_and_HSV
+
     [Required]
     public string FirstColor { get; set; }
 
@@ -17,7 +18,12 @@ public class ColorInterpolation: Color
     
     public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
     {
-        throw new NotImplementedException();
+        int max = Math.Max((byte)color.red, Math.Max((byte)color.green, (byte)color.blue));
+        int min = Math.Min((byte)color.red, Math.Min((byte)color.green, (byte)color.blue));
+
+        hue = color.GetHue();
+        saturation = Math.Round(((max == 0) ? 0 : ((max / 255d) - (min / 255d)) / (max / 255d)) * 100, 1);
+        value = Math.Round((max / 255d) * 100, 1);
     }
 
     public static Color ColorFromHSV(double hue, double saturation, double value)
--- Test/ColorInterpolationModelValidator_Tests.cs
+++ Test/ColorInterpolationModelValidator_Tests.cs
@@ -134,4 +134,76 @@ public class ColorInterpolationModelValidator_Tests
             Assert.That(mv.ContainsFailureFor("NumColors"), Is.True);
         });
     }
+
+    [Test]
+    public void ValidColorInterpolation_CalculateHSVForColorYellow_ShouldReturnCorrectValues()
+    {
+        // Arrange
+        Color newColor = new()
+        {
+            red = 255,
+            green = 255,
+            blue = 0
+        };
+        double hue, saturation, value;
+
+        // Act
+        ColorInterpolation.ColorToHSV(newColor, out hue, out saturation, out value);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(hue, Is.EqualTo(60).Within(0.075));
+            Assert.That(saturation, Is.EqualTo(100).Within(0.075));
+            Assert.That(value, Is.EqualTo(100).Within(0.075));
+        });
+    }
+
+    [Test]
+    public void ValidColorInterpolation_CalculateHSVForColorDarkGreen_ShouldReturnCorrectValues()
+    {
+        // Arrange
+        Color newColor = new()
+        {
+            red = 29,
+            green = 38,
+            blue = 29
+        };
+        double hue, saturation, value;
+
+        // Act
+        ColorInterpolation.ColorToHSV(newColor, out hue, out saturation, out value);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(hue, Is.EqualTo(120).Within(0.075));
+            Assert.That(saturation, Is.EqualTo(23.69).Within(0.075));
+            Assert.That(value, Is.EqualTo(14.9).Within(0.075));
+        });
+    }
+
+    [Test]
+    public void ValidColorInterpolation_CalculateHSVForColorWhite_ShouldReturnCorrectValues()
+    {
+        // Arrange
+        Color newColor = new()
+        {
+            red = 242,
+            green = 242,
+            blue = 242
+        };
+        double hue, saturation, value;
+
+        // Act
+        ColorInterpolation.ColorToHSV(newColor, out hue, out saturation, out value);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(hue, Is.EqualTo(0).Within(0.075));
+            Assert.That(saturation, Is.EqualTo(0).Within(0.075));
+            Assert.That(value, Is.EqualTo(94.9).Within(0.075));
+        });
+    }
 }
