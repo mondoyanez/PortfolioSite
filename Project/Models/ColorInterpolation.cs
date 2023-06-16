7b840fb3f09715146f9d174e862926df2df3d00e
Armando Yanez <ayanez20@wou.edu>
2023-06-15 21:25:13 -0700

    Implementated and tested properties in ColorInterpolation class and added necessary DataAnnotations to properties in class

--- Project/Models/ColorInterpolation.cs
+++ Project/Models/ColorInterpolation.cs
@@ -1,14 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Portfolio.Models;
 
 public class ColorInterpolation: Color
 {
     // From Greg's answer: https://stackoverflow.com/questions/359612/how-to-change-rgb-color-to-hsv/1626175
     // And Wikipedia: https://en.wikipedia.org/wiki/HSL_and_HSV
-        
+    [Required]
     public string FirstColor { get; set; }
 
+    [Required]
     public string SecondColor { get; set; }
 
+    [Range(2, 50)]
     public int? NumColors { get; set; }
     
     public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
--- Test/ColorInterpolationModelValidator_Tests.cs
+++ Test/ColorInterpolationModelValidator_Tests.cs
@@ -26,6 +26,112 @@ public class ColorInterpolationModelValidator_Tests
         ModelValidator mv = new ModelValidator(colorInterpolation);
 
         // Assert
-        Assert.That(mv.Valid, Is.True);
-    }    
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.True);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.False);
+        });
+    }
+
+    [Test]
+    public void InvalidFirstColorInterpolation_IsNotValid()
+    {
+        // Arrange
+        ColorInterpolation colorInterpolation = MakeValidColorInterpolation();
+        colorInterpolation.FirstColor = null;
+
+        // Act
+        ModelValidator mv = new ModelValidator(colorInterpolation);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.False);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.True);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.False);
+        });
+    }
+
+    [Test]
+    public void InvalidSecondColorInterpolation_IsNotValid()
+    {
+        // Arrange
+        ColorInterpolation colorInterpolation = MakeValidColorInterpolation();
+        colorInterpolation.SecondColor = null;
+
+        // Act
+        ModelValidator mv = new ModelValidator(colorInterpolation);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.False);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.True);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.False);
+        });
+    }
+
+    [Test]
+    public void ValidNumColorsInterpolation_IsValid()
+    {
+        // Arrange
+        ColorInterpolation colorInterpolation = MakeValidColorInterpolation();
+        colorInterpolation.NumColors = null;
+
+        // Act
+        ModelValidator mv = new ModelValidator(colorInterpolation);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.True);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.False);
+        });
+    }
+
+    [Test]
+    public void InvalidNumColorsInterpolation_WithNegativeNumColors_IsNotValid()
+    {
+        // Arrange
+        ColorInterpolation colorInterpolation = MakeValidColorInterpolation();
+        colorInterpolation.NumColors = -13;
+
+        // Act
+        ModelValidator mv = new ModelValidator(colorInterpolation);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.False);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.True);
+        });
+    }
+
+    [Test]
+    public void InvalidNumColorsInterpolation_WithNumColorsExceedingRange_IsNotValid()
+    {
+        // Arrange
+        ColorInterpolation colorInterpolation = MakeValidColorInterpolation();
+        colorInterpolation.NumColors = 151;
+
+        // Act
+        ModelValidator mv = new ModelValidator(colorInterpolation);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(mv.Valid, Is.False);
+            Assert.That(mv.ContainsFailureFor("FirstColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("SecondColor"), Is.False);
+            Assert.That(mv.ContainsFailureFor("NumColors"), Is.True);
+        });
+    }
 }
