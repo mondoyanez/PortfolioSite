62d85662e0c65cfa4146621d5329b2b865ca38fc
Armando Yanez <ayanez20@wou.edu>
2023-06-22 16:13:38 -0700

    Created view model for madlibs, implementated action method Display story and started implementating DisplayStory view

--- Project/Controllers/MadLibsController.cs
+++ Project/Controllers/MadLibsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Portfolio.Models;
+using Portfolio.ViewModels;
 
 namespace Portfolio.Controllers;
 
@@ -13,7 +14,76 @@ public class MadLibsController : Controller
     [HttpGet]
     public IActionResult DisplayStory(List<string> words, string story)
     {
-        return View();
+        RoadTripMadLib roadTripMadLib = new();
+        CampMadLib campMadLib = new();
+
+        if (story == "Road Trip")
+        {
+            RoadTripMadLib temp = new()
+            {
+                Noun1 = words[0],
+                Adjective1 = words[1],
+                Noun2 = words[2],
+                Adjective2 = words[3],
+                Adjective3 = words[4],
+                Verb1 = words[5],
+                Verb2 = words[6],
+                Number1 = Convert.ToInt32(words[7]),
+                Verb3 = words[8],
+                Adverb1 = words[9],
+                Noun3 = words[10],
+                Color1 = words[11],
+                Verb4 = words[12],
+                Noun4 = words[13],
+                Exclamation1 = words[14],
+                Verb5 = words[15],
+                Adverb2 = words[16],
+                Noun5 = words[17],
+                Verb6 = words[18],
+                Verb7 = words[19],
+                Noun6 = words[20],
+                PluralNoun1 = words[21],
+                Verb8 = words[22],
+                Name1 = words[23],
+                Verb9 = words[24],
+                Noun7 = words[25],
+                Verb10 = words[26],
+                Adjective4 = words[27],
+                Adverb3 = words[28],
+                Adverb4 = words[29],
+                Noun8 = words[30],
+                Verb11 = words[31],
+                Noun9 = words[32],
+                Adjective5 = words[33]
+            };
+            roadTripMadLib = temp;
+        }
+
+        if (story == "Camping")
+        {
+            CampMadLib temp = new()
+            {
+                Name1 = words[0],
+                CampName1 = words[1],
+                Adjective1 = words[2],
+                Activity1 = words[3],
+                Activity2 = words[4],
+                PluralNoun1 = words[5],
+                Adjective2 = words[6],
+                Noun1 = words[7],
+                NickName1 = words[8]
+            };
+            campMadLib = temp;
+        }
+
+        MadLibsVM vm = new()
+        {
+            StoryTitle = story,
+            roadTripMadLib = roadTripMadLib,
+            campMadLib = campMadLib
+        };
+
+        return View(vm);
     }
 
     [HttpGet]
