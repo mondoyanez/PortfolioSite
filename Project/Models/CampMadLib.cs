a6a1496d36cb4eb56792cab081e33ebc0d75cc14
Armando Yanez <ayanez20@wou.edu>
2023-06-21 18:46:35 -0700

    Created models that will be used for Mab lib stories

---
+++ Project/Models/CampMadLib.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models;
+
+public class CampMadLib
+{
+    [Required]
+    public string Name1 { get; set; }
+    [Required]
+    public string CampName { get; set; }
+    [Required]
+    public string Adjective1 { get; set; }
+    [Required]
+    public string Activity1 { get; set; }
+    [Required]
+    public string Activity2 { get; set; }
+    [Required]
+    public string PluralNoun1 { get; set; }
+    [Required]
+    public string Adjective2 { get; set; }
+    [Required]
+    public string Noun1 { get; set; }
+    [Required]
+    public string NickName1 { get; set; }
+}
