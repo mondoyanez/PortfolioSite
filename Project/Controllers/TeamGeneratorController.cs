9e4dc2beb21fa5234e431b1ce7a30e62d0f7b303
Armando Yanez <ayanez20@wou.edu>
2023-07-16 16:23:21 -0700

    beginning generating of teams in GenerateTeams view

--- Project/Controllers/TeamGeneratorController.cs
+++ Project/Controllers/TeamGeneratorController.cs
@@ -1,16 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
 
 namespace Portfolio.Controllers;
 
 public class TeamGeneratorController : Controller
 {
-    public TeamGeneratorController()
-    {
-
-    }
-
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult GenerateTeams(TeamsModel teams)
+    {
+        ViewBag.IsValid = false;
+
+        if (!ModelState.IsValid) return View("Index");
+
+        ViewBag.IsValid = true;
+        return View("GenerateTeams", teams);
     }
 }
