a08f6a1c4046b51505f092f03e811256eb610901
Armando Yanez <ayanez20@wou.edu>
2023-10-30 21:59:17 -0700

    fixed issue with jQuery not working due to missing script tag in shared view directory

--- Project/wwwroot/js/githubApi.js
+++ Project/wwwroot/js/githubApi.js
@@ -1,1 +1,25 @@
 console.log("hello world from GitHub Api javascript file");
+
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "/api/user",
+    success: populateGitHubUserData,
+    error: errorOnAjax
+});
+
+function populateGitHubUserData(data) {
+    /*
+    $("#github-user-image").attr("src", data["avatarURL"]);
+    $("#github-user-info").append(`<h1 id="name-header"> ${data["name"]} </h1>`);
+    $("#github-user-info").append(`<p id="github-account-info"> ${data["userName"]}`);
+    $("#github-user-info").append(`<p id="github-account-info"> ${data["email"]}`);
+    $("#github-user-info").append(`<p id="github-account-info"> ${data["company"]}`);
+    $("#github-user-info").append(`<p id="github-account-info"> ${data["location"]}`);
+    */
+   console.log(data);
+}
+
+function errorOnAjax() {
+    console.log("ERROR in ajax request");
+}
