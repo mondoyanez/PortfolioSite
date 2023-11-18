26489ff061b741af2d262aa0ccaf44db306a18d1
Armando Yanez <ayanez20@wou.edu>
2023-11-17 21:16:32 -0800

    Now getting all public repos from user and turning them into buttons that function with light and dark mode and removed console logs that where used for javascript testing

--- Project/wwwroot/js/githubApi.js
+++ Project/wwwroot/js/githubApi.js
@@ -1,5 +1,3 @@
-console.log("hello world from GitHub Api javascript file");
-
 $.ajax({
     type: "GET",
     dataType: "json",
@@ -8,6 +6,25 @@ $.ajax({
     error: errorOnAjax
 });
 
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "/api/repositories",
+    success: populateGitHubRepoData,
+    error: errorOnAjax
+});
+
+function populateGitHubRepoData(data) {
+    for (let i = 0; i < data.length; ++i) {
+        let repoTR = 
+        `<tr>
+            <td><button class="my-2 bg-blue-100 dark:bg-blue-900 text-sm font-serif font-bold text-blue-800 dark:text-blue-200 rounded-md p-1.5 hover:bg-yellow-200 dark:hover:bg-yellow-800" id="${data[i]["name"]}" value=${data[i]["fullName"]} type="button">${data[i]["fullName"]}</button></td>
+        </tr>`;
+        $("#repository-names").append((repoTR));
+        $("#repository-names").show();
+    }
+}
+
 function populateGitHubUserData(data) {
     $("#github-user-image").attr("src", data["avatarURL"]);
     $("#name-header").html(data["name"]);
@@ -15,7 +32,6 @@ function populateGitHubUserData(data) {
     $("#github-email").html(data["email"]);
     $("#github-company").html(data["company"]);
     $("#github-location").html(data["location"]);
-    console.log(data);
 }
 
 function errorOnAjax() {
